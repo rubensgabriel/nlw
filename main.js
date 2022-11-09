b4761a227d2d6edfb2c9e16906f9150b70fd93e8
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-11-09 18:51:09 -0300

    3º Etapa parte 2

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
       <strong>${hour}</strong>
-      <img src="assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
     </li>
   `
 }
@@ -21,11 +21,11 @@ function createCard(date, day , games) {
 
 document.querySelector("#app").innerHTML = `
   <header>
-      <img src="assets/logo.svg" alt="">
+      <img src="assets/logo.svg" alt="Logo da NLW">
   </header>
   <main id="cards">
     ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-    ${createCarc("28/11", "segunda", createGame("brazil", "13:00", "switzerland"))}
-    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "serbia"))}
+    ${createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay"))}
+    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
   </main>
 `
